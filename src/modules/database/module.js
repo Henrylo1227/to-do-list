7e4fcdec1b24c8ddb58ed656d9750d6fa3ccb9d3
Henrylo1227 <52083095+Henrylo1227@users.noreply.github.com>
2023-06-07 18:08:51 +0100

    Revert "use temp.js to experiment on sqlite3 database operations."
    This reverts commit fb716ebfe04f21e58796b5c751281c418f3abc89.

--- src/modules/database/module.js
+++ src/modules/database/module.js
@@ -1,33 +1,23 @@
-const sqlite3 = require('sqlite3').verbose();
+const sqlite3 = require('sqlite3');
 
-// specifiy the database path and mode option
-const dbPath = './db/database.db';
-const dbModeOptions = sqlite3.OPEN_CREATE | sqlite3.OPEN_READWRITE;
 
-exports.CreateDBConnection = () => {
-    // Create a new database connection
-    const db = new sqlite3.Database(
-        dbPath,
-        dbModeOptions, 
-        (error) => {
-            if (error) {
-                console.error(`CreateDBConnection: connection failed: ${error.msg}`);
-                return;
-            }
-    });
-    console.log('database opened successfully');
+export const CreateDBConnection = () => {
 
-    // Create table if the table is not exist
+    const db = new sqlite3.oo1.DB();
     try {
-        let sql
-        //create TABLE_TASK            
-        sql=`create table if not exists TABLE_TASK (
-                    task_id char(4) primary key,
-                    check_status bool,
-                    description varchar(20));`;
-        db.run(sql, (error) => {
-            if (error) return console.error(`CreateDBConnection: create table failed: ${error.msg}`);
-        });
+        db.exec([
+            `
+            create table if not exists TABLE_TASK (
+                task_id char(4),
+                check_status bool,
+        description varchar(20));
+        `,
+        `
+        insert into TABLE_TASK values (
+            '0', 
+            false, 
+            'demo description');`
+        ]);
     } catch(error){
         console.error(error);
     } finally {
@@ -35,52 +25,18 @@ exports.CreateDBConnection = () => {
     }
 }
 
-exports.ExtractAllRecordFromDB = () => { 
-    const db = new sqlite3.Database(
-        dbPath, 
-        dbModeOptions,
-        (error) => {
-        if (error) {
-            console.error(`ExtractAllRecordFromDB: operation failed: ${error.message}`);
-            return; 
-        }    
-    });
+export const ExtractRecordFromDB = () => { 
+
+    const db = new sqlite3.oo1.DB();
     try {
-        db.all("select * from TABLE_TASK order by task_id",
-        (error, rows) => {
-            rows.forEach( row => console.log(` 
-                                                task_id:${row.task_id}, 
-                                                check_status:${row.check_status}, 
-                                                description: ${row.description}`))
-            
-        });
+        db.exec([
+            `
+            select * from TASK_TABLE where task_id = '0';
+            `,
+        ]);
     } catch(error){
         console.error(error);
     } finally {
         db.close();
     }
-}
-
-exports.InsertSampleData = () => {
-    const db = new sqlite3.Database(
-        dbPath,
-        dbModeOptions,
-        (error) => {
-            if (error) {
-                console.error(`ExtractAllRecordFromDB: operation failed: ${error.message}`);
-                return; 
-            }    
-        });
-    try{
-        //create sample data
-        db.run('insert into TABLE_TASK values ("0", false, "demo description task1");');
-        db.run('insert into TABLE_TASK values ("1", false, "demo description task2");');
-        db.run('insert into TABLE_TASK values ("2", false, "demo description task3");');
-        db.run('insert into TABLE_TASK values ("3", false, "demo description task4");');
-    } catch (error) {
-        console.error(`InsertSampleData: Insertion failed: ${error.message}`);
-    } finally {
-        db.close();
-    }
-    console.log('InsertSampleData: Insertion completed');
 }
