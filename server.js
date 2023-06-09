e232dbf05982bf0bc53f18281abea26e0860e347
Henrylo1227 <52083095+Henrylo1227@users.noreply.github.com>
2023-06-09 17:50:49 +0100

    add new module axios.
    use axios to post get request from server (get datafrom database).

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const { InitDb } = require('./src/modules/database/DatabaseModule')
+const { InitDb, ExtractAllFromTable } = require('./src/modules/database/DatabaseModule')
 
 const { log } = require('console');
 const express = require('express');
 
 InitDb();
-ServerStart();
+Server();
 
-function ServerStart() {
+function Server() {
   //server setting
   const hostname = '127.0.0.1';
   const port = 3000;
@@ -20,23 +20,33 @@ function ServerStart() {
 
   // Define routes for the different views
   app.get('/', (req, res) => {
-    console.log('ServerStart: "GET/" request received...');
+    console.log('Server: "GET/" request received...');
     const filePath = path.join(__dirname, 'public', 'dist', 'index.html');
     res.set({
       'Content-Type': 'text/html'
     })
     res.sendFile(filePath);
-    console.log(`ServerStart: responsed file ${filePath}`);
+    console.log(`Server: responsed file ${filePath}`);
   });
 
   app.get('/main.js', (req, res) => {
-    console.log('ServerStart: "GET/main.js" request received...');
+    console.log('Server: "GET/main.js" request received...');
     const filePath = path.join(__dirname, 'public', 'dist', 'main.js'); // "./public/dist/main.js"
     res.set({
       'Content-Type': 'application/javascript'
     })
     res.sendFile(filePath);
-    console.log(`ServerStart: responsed file ${filePath}`);
+    console.log(`Server: responsed file ${filePath}`);
+  });
+
+  app.get('/data/taskTable', async (req, res) => {
+    console.log('Server: "GET/data/taskTable.js" request received...');
+    const dataJson = await ExtractAllFromTable('TABLE_TASK');
+    res.set({
+      'Content-Type': 'application/json'
+    })
+    res.send(dataJson);
+    console.log(`Server: responsed content: ${dataJson}`);
   });
 
   // Start the server
--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const {TableInit} = require('./modules/taskTable/TaskTableModule');
+const axios = require('./../node_modules/axios/dist/browser/axios.cjs');
 
-//fetch data
-fetch()
+// Make request to fetch data taskTable
+axios('/data/taskTable');
 
+
+// User Interface
 TableInit();
